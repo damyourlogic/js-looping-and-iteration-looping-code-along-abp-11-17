5a742478691cc31b26334cd277ca387dcbb94da9
Amy Jackson <aj81623@gmail.com>
2017-11-11 18:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+  const printBadgeName =['Ada', 'Brendan', 'Ali']
+ function printBadges(name){
+  for (let x = 0; x < name.length; x++) {
+    console.log(`Welcome ${name[x]}! You are employee #${x+1}.`)
+  }
+return name;
+}
